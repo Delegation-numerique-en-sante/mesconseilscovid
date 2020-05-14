a2f18701f534c5f00de495aaad400f5bf6ef410b
David Larlet <david@larlet.fr>
2020-05-13 21:03:07 -0400

    Tech: évite l’usage de fetch pour la compat IE11

--- src/main.js
+++ src/main.js
@@ -701,7 +701,7 @@ var Geolocaliseur = function () {
     this.matchDepartement = function (lat, lon, callback) {
         // Warning, in case of multiple polygons, you can have multiple matches.
         var that = this
-        this.loadMap().then(function (featureCollection) {
+        this.loadMap(function (featureCollection) {
             featureCollection.features.forEach(function (departement) {
                 if (departement.geometry.type === 'Polygon') {
                     var polyCoordinates = [departement.geometry.coordinates]
@@ -719,10 +719,15 @@ var Geolocaliseur = function () {
         })
     }
 
-    this.loadMap = function () {
-        return fetch('departements-1000m.geojson').then(function (response) {
-            return response.json()
-        })
+    this.loadMap = function (callback) {
+        var xhr = new XMLHttpRequest()
+        xhr.overrideMimeType('application/json')
+        xhr.open('GET', 'departements-1000m.geojson', true)
+        xhr.onload = function () {
+            var jsonResponse = JSON.parse(xhr.responseText)
+            callback(jsonResponse)
+        }
+        xhr.send()
     }
 
     this.booleanPointInPolygon = function (point, vs) {
@@ -751,15 +756,13 @@ function geolocalisation(event) {
     event.preventDefault()
     navigator.geolocation.getCurrentPosition(
         function (pos) {
-            geolocaliseur.matchDepartement(
-                pos.coords.latitude,
-                pos.coords.longitude,
-                function (dpt) {
-                    if (typeof dpt !== 'undefined') {
-                        document.getElementById('departement').value = dpt.code
-                    }
+            var latitude = pos.coords.latitude
+            var longitude = pos.coords.longitude
+            geolocaliseur.matchDepartement(latitude, longitude, function (dpt) {
+                if (typeof dpt !== 'undefined') {
+                    document.getElementById('departement').value = dpt.code
                 }
-            )
+            })
         },
         function (err) {
             console.warn('ERREUR (' + err.code + '): ' + err.message)
