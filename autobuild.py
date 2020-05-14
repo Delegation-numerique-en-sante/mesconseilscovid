b63c74e7bb6af5a895b147be7ab0997baf8ea578
David Larlet <david@larlet.fr>
2020-05-14 08:52:15 -0400

    Tech(bug): correction des boutons retours
    Introduit lors de https://github.com/Delegation-numerique-en-sante/mesconseilscovid/pull/12

--- autobuild.py
+++ autobuild.py
@@ -2,4 +2,5 @@ from livereload import Server, shell
 
 server = Server()
 server.watch("contenus/*.md", shell("make build"))
+server.watch("src/template.html", shell("make build"))
 server.serve(root="src/")
