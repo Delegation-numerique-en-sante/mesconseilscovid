15e13f924d7821aab27beddb8000eb41c0b5cae1
Ronan Amicel <ronan.amicel@gmail.com>
2020-05-25 15:44:30 +0200

    Tech: ajoute un polyfill pour CustomEvent sur Internet Explorer
    https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+// Polyfill for CustomEvent support on IE9+
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+;(function () {
+    if (typeof window.CustomEvent === 'function') return false
+
+    function CustomEvent(event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: null }
+        var evt = document.createEvent('CustomEvent')
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail)
+        return evt
+    }
+
+    window.CustomEvent = CustomEvent
+})()
+
 var Affichage = function () {
     this.hideElement = function (element) {
         element.setAttribute('hidden', '')
