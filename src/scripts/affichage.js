ef4ca0706cad974a34907b00ff70a840165e0ae5
Ronan Amicel <ronan.amicel@gmail.com>
2020-05-27 17:49:36 +0200

    Tech: `element.getRootNode()` n’est pas disponible sur Internet Explorer

--- src/scripts/affichage.js
+++ src/scripts/affichage.js
@@ -12,10 +12,22 @@ function displayElement(element, id) {
     block.removeAttribute('hidden')
     block.classList.add('visible')
 
-    var document_ = element.getRootNode()
+    // We avoid using the `document` global, as it is not available
+    // when running unit tests in Node.
+    // We also cannot use `element.getRootNode()` as it is not available
+    // in Internet Explorer.
+    var document_ = getRoot(element)
+
     var customDisplayEvent = document_.createEvent('CustomEvent')
     customDisplayEvent.initCustomEvent('elementDisplayed:' + id, true, true, block)
     document_.dispatchEvent(customDisplayEvent)
+}
+
+function getRoot(node) {
+    if (node.parentNode != null) {
+        return getRoot(node.parentNode)
+    }
+    return node
 }
 
 function displayBlocks(element, blockNames) {
