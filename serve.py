83bdcecd1f05a64e9de061ace6d3d99f9bf2b5f9
Ronan Amicel <ronan.amicel@gmail.com>
2020-05-29 12:59:20 +0200

    Tech: unifie les serveurs locaux pour le développement
    Une même commande permet de lancer :
    - soit un serveur HTTP (pour le support de LiveReload)
    - soit un serveur HTTPS (pour le support de la géolocalisation)
    
    Dans les deux cas, une reconstruction du site est déclenchée
    automatiquement en case de modification d’un fichier source.

--- serve.py
+++ serve.py
@@ -1,18 +1,89 @@
+"""
+Start local development server
+"""
+import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
-import ssl
+from ssl import wrap_socket
+
+from livereload import Server, shell
+from watchdog.observers import Observer
+from watchdog.tricks import ShellCommandTrick
 
 
-ADDRESS = "0.0.0.0"
-PORT = 8443
+ROOT_DIR = "dist/"
 
-class Handler(SimpleHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="dist", **kwargs)
+WATCHED_PATHS = [
+    "contenus/**/[!README]*.md",
+    "src/scripts/**/*.js",
+    "src/style.css",
+    "src/template.html",
+    "src/version.json",
+]
+
+COMMAND = "make build"
 
 
-httpd = HTTPServer((ADDRESS, PORT), Handler)
-httpd.socket = ssl.wrap_socket(
-    httpd.socket, keyfile="key.pem", certfile="cert.pem", server_side=True
-)
-print(f"Listening on https://{ADDRESS}:{PORT}/")
-httpd.serve_forever()
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--address", default="0.0.0.0")
+    parser.add_argument("--port", type=int, default=None)
+    parser.add_argument("--ssl", action="store_true")
+    parser.add_argument("--ssl-cert", default="cert.pem")
+    parser.add_argument("--ssl-key", default="key.pem")
+    parser.add_argument("--watch", action="store_true")
+    return parser.parse_args()
+
+
+def serve(address, port, watch):
+    server = Server()
+    if watch:
+        for path in WATCHED_PATHS:
+            server.watch(path, shell(COMMAND))
+    server.serve(host=address, port=port, root=ROOT_DIR)
+
+
+def serve_ssl(address, port, ssl_cert, ssl_key, watch):
+    class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=ROOT_DIR, **kwargs)
+
+    class MyFileSystemEventHandler(ShellCommandTrick):
+        def __init__(self):
+            super().__init__(
+                shell_command=COMMAND,
+                patterns=WATCHED_PATHS,
+                wait_for_process=True,
+                drop_during_process=True,
+            )
+
+        def on_any_event(self, event):
+            print(event)
+            super().on_any_event(event)
+
+    if watch:
+        observer = Observer()
+        handler = MyFileSystemEventHandler()
+        for path in ["src", "contenus"]:
+            observer.schedule(handler, path, recursive=True)
+        observer.start()
+
+    httpd = HTTPServer((address, port), MyHTTPRequestHandler)
+    httpd.socket = wrap_socket(
+        httpd.socket, certfile=ssl_cert, keyfile=ssl_key, server_side=True
+    )
+    print(f"Listening on https://{address}:{port}/")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    if args.ssl:
+        serve_ssl(
+            address=args.address,
+            port=args.port or 8443,
+            watch=args.watch,
+            ssl_cert=args.ssl_cert,
+            ssl_key=args.ssl_key,
+        )
+    else:
+        serve(address=args.address, port=args.port or 5500, watch=args.watch)
