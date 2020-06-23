0844bbfc8ee27dd04a8595434bf362e1414192c0
Ronan Amicel <ronan.amicel@gmail.com>
2020-06-23 15:58:25 +0200

    Tech: le service worker (PWA) garde les assets en cache

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,9 +1,31 @@
 /* Implementation of the network-or-cache pattern:
 https://serviceworke.rs/strategy-network-or-cache.html */
-const CACHE_NAME = 'network-or-cache-2020-06-15'
+const CACHE_NAME = 'network-or-cache-2020-06-23'
 const CACHE_FILES = [
-    './index.html',
-    // TODO: add more.
+    '/',
+    'style.css',
+    'fonts/marianne-regular-webfont.woff2',
+    'fonts/marianne-bold-webfont.woff2',
+    'scripts/main.js',
+    'marianne.png',
+    'arrow-left.svg',
+    'map-marker2.svg',
+    'printer.svg',
+    'select.svg',
+    'star.svg',
+    'trash.svg',
+    'illustrations/accueil.svg',
+    'illustrations/activitepro.svg',
+    'illustrations/antecedents.svg',
+    'illustrations/caracteristiques.svg',
+    'illustrations/contactarisque.svg',
+    'illustrations/foyer.svg',
+    'illustrations/pediatrieecole.svg',
+    'illustrations/pediatriegeneral.svg',
+    'illustrations/pediatriemedical.svg',
+    'illustrations/residence.svg',
+    'illustrations/symptomesactuels.svg',
+    'illustrations/symptomespasses.svg',
 ]
 const TIMEOUT = 500 // ms.
 
