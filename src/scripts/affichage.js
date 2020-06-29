55b3482bfd45f8ea8ef0c3c224756a20a5011308
David Larlet <david@larlet.fr>
2020-06-29 15:12:25 -0400

    Tech: émission d’un customevent que pour la bannière

--- src/scripts/affichage.js
+++ src/scripts/affichage.js
@@ -17,23 +17,6 @@ function displayElementById(element, id) {
     var block = element.querySelector('#' + id)
     if (!block) return
     showElement(block)
-
-    // We avoid using the `document` global, as it is not available
-    // when running unit tests in Node.
-    // We also cannot use `element.getRootNode()` as it is not available
-    // in Internet Explorer.
-    var document_ = getRoot(element)
-
-    var customDisplayEvent = document_.createEvent('CustomEvent')
-    customDisplayEvent.initCustomEvent('elementDisplayed:' + id, true, true, block)
-    document_.dispatchEvent(customDisplayEvent)
-}
-
-function getRoot(node) {
-    if (node.parentNode != null) {
-        return getRoot(node.parentNode)
-    }
-    return node
 }
 
 function displayBlocks(element, blockNames) {
