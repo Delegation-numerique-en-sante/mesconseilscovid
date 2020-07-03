51b7d1376888b2cbc7a44b0598f20fd62ea62ea9
Ronan Amicel <ronan.amicel@gmail.com>
2020-07-03 16:22:26 +0200

    Tech: ajoute un scénario de test qui remplit tout le questionnaire

--- src/scripts/tests/test.browser.js
+++ src/scripts/tests/test.browser.js
@@ -46,4 +46,134 @@ describe('Scénarios navigateur', function () {
             'Mes conseils Covid — Des conseils personnalisés pour agir contre le virus'
         )
     })
+
+    it('remplir le questionnaire', async () => {
+        // On est redirigé vers l’introduction
+        await Promise.all([
+            page.goto('http://localhost:8080/'),
+            page.waitForNavigation({ url: '**/#introduction' }),
+        ])
+
+        // Page d’accueil
+        {
+            let bouton = await page.waitForSelector('#page >> text="Démarrer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#residence' }),
+            ])
+        }
+
+        // Questionnaire 1/8
+        {
+            let titre = await page.$('#page legend')
+            assert.equal(await titre.innerText(), '1/8 - Mon lieu de résidence')
+            await page.selectOption('#page select#departement', '80')
+            let bouton = await page.waitForSelector('#page >> text="Continuer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#activitepro' }),
+            ])
+        }
+
+        // Questionnaire 2/8
+        {
+            // Je n’arrive pas à cocher la case directement, alors je clique sur le label
+            let label
+            label = await page.waitForSelector('#page label[for="activite_pro"]')
+            await label.click()
+            let bouton = await page.waitForSelector('#page >> text="Continuer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#foyer' }),
+            ])
+        }
+
+        // Questionnaire 3/8
+        {
+            // Je n’arrive pas à cocher la case directement, alors je clique sur le label
+            let label
+            label = await page.waitForSelector('#page label[for="foyer_enfants"]')
+            await label.click()
+            let bouton = await page.waitForSelector('#page >> text="Continuer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#caracteristiques' }),
+            ])
+        }
+
+        // Questionnaire 4/8
+        {
+            await page.fill('#page #age', '42')
+            await page.fill('#page #taille', '165')
+            await page.fill('#page #poids', '70')
+            let bouton = await page.waitForSelector('#page >> text="Continuer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#antecedents' }),
+            ])
+        }
+
+        // Questionnaire 5/8
+        {
+            let bouton = await page.waitForSelector(
+                '#page >> text="Aucun de ces éléments ne correspond à ma situation"'
+            )
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#symptomesactuels' }),
+            ])
+        }
+
+        // Questionnaire 6/8
+        {
+            let bouton = await page.waitForSelector(
+                '#page >> text="Je n’ai pas de symptômes actuellement"'
+            )
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#symptomespasses' }),
+            ])
+        }
+
+        // Questionnaire 7/8
+        {
+            let titre = await page.$('#page legend')
+            assert.equal(
+                await titre.innerText(),
+                '7/8 - Mon état ces 14 derniers jours'
+            )
+            let bouton = await page.waitForSelector(
+                '#page >> text="Je n’ai pas eu de symptômes dans les 14 derniers jours"' // &nbsp; après le 14
+            )
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#contactarisque' }),
+            ])
+        }
+
+        // Questionnaire 8/8
+        {
+            let titre = await page.$('#page legend')
+            assert.equal(await titre.innerText(), '8/8 - Mes contacts récents')
+            let bouton = await page.waitForSelector('#page >> text="Terminer"')
+            await Promise.all([
+                bouton.click(),
+                page.waitForNavigation({ url: '**/#conseils' }),
+            ])
+        }
+
+        // Conseils
+        {
+            // On retrouve le département de résidence
+            let residence = await page.$('#page #nom-departement')
+            assert.equal(await residence.innerText(), 'Somme')
+
+            // On retrouve l’activité
+            let activite = await page.$('#page #reponse-activite-pro')
+            assert.equal(
+                await activite.innerText(),
+                'Vous exercez une activité professionnelle et/ou bénévole (modifier)'
+            )
+        }
+    })
 })
