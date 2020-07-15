296c3c4ef7582b88bfa86a1abd2cef4812247164
Ronan Amicel <ronan.amicel@gmail.com>
2020-07-15 18:52:02 +0200

    Ajoute le choix de la date exacte de début des symptômes

--- src/scripts/page/questionnaire.js
+++ src/scripts/page/questionnaire.js
@@ -287,9 +287,14 @@ function contactarisque(form, app, router) {
 }
 
 function suividate(form, app, router) {
-    function dateFromString(string) {
+    function dateFromPicker(element) {
+        if (element.value !== '') {
+            return new Date(element.value)
+        }
+    }
+    function dateFromRadioButton(element) {
         let delta
-        switch (string) {
+        switch (element.value) {
             case 'aujourdhui':
                 delta = 0
                 break
@@ -302,10 +307,6 @@ function suividate(form, app, router) {
             case 'avant_avant_hier':
                 delta = 3
                 break
-            case 'encore_avant_hier':
-                // Que faire ?
-                delta = 4
-                break
         }
         const today = new Date()
         return new Date(today.setDate(today.getDate() - delta))
@@ -317,11 +318,29 @@ function suividate(form, app, router) {
         app.enregistrerProfilActuel()
     }
 
+    function radioButtonChanged(event) {
+        let datePicker = form.querySelector('#suivi_date_exacte')
+        if (event.target.value === 'encore_avant_hier') {
+            datePicker.removeAttribute('disabled')
+            datePicker.setAttribute('max', new Date().toISOString().slice(0, 10))
+            datePicker.focus()
+        } else {
+            datePicker.setAttribute('disabled', '')
+        }
+    }
+    // eslint-disable-next-line no-extra-semi
+    ;[].forEach.call(
+        form.querySelectorAll('[name="suivi_symptomes_date"]'),
+        (radio) => {
+            radio.addEventListener('change', radioButtonChanged)
+        }
+    )
+
     form.addEventListener('submit', function (event) {
         event.preventDefault()
-        app.profil.symptomes_start_date = dateFromString(
-            event.target.elements['suivi_symptomes_date'].value
-        )
+        app.profil.symptomes_start_date =
+            dateFromPicker(event.target.elements['suivi_symptomes_date_exacte']) ||
+            dateFromRadioButton(event.target.elements['suivi_symptomes_date'])
         app.enregistrerProfilActuel()
         router.navigate('suivisymptomes')
     })
