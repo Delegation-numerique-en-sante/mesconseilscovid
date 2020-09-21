cef85acc1a88a550a3008d66ba76c7af64b31d77
Ronan Amicel <ronan.amicel@gmail.com>
2020-09-21 16:27:20 +0200

    Tech: représente la séquence de parcours inverse du questionnaire

--- src/scripts/questionnaire.js
+++ src/scripts/questionnaire.js
@@ -1,17 +1,22 @@
 const questions = {
     nom: {
+        previous: () => 'introduction',
         next: () => 'residence',
     },
     residence: {
+        previous: () => 'introduction',
         next: () => 'foyer',
     },
     foyer: {
+        previous: () => 'residence',
         next: () => 'antecedents',
     },
     antecedents: {
+        previous: () => 'foyer',
         next: () => 'caracteristiques',
     },
     caracteristiques: {
+        previous: () => 'antecedents',
         next: (profil) => {
             if (profil.age < 15) {
                 return 'pediatrie'
@@ -21,9 +26,11 @@ const questions = {
         },
     },
     activitepro: {
+        previous: () => 'caracteristiques',
         next: () => 'symptomesactuels',
     },
     symptomesactuels: {
+        previous: () => 'activitepro',
         next: (profil) => {
             if (profil.symptomes_actuels && !profil.symptomes_actuels_autre) {
                 if (profil.symptomes_start_date) {
@@ -37,6 +44,7 @@ const questions = {
         },
     },
     symptomespasses: {
+        previous: () => 'symptomesactuels',
         next: (profil) => {
             if (profil.symptomes_passes) {
                 return 'conseils'
@@ -46,12 +54,15 @@ const questions = {
         },
     },
     contactarisque: {
+        previous: () => 'symptomespasses',
         next: () => 'conseils',
     },
     suividate: {
+        previous: () => 'suiviintroduction',
         next: () => 'suivisymptomes',
     },
     suivisymptomes: {
+        previous: () => 'suiviintroduction',
         next: () => 'conseils',
     },
     conseils: {},
@@ -61,6 +72,10 @@ class Questionnaire {
     nextPage(currentPage, profil) {
         const question = questions[currentPage]
         if (typeof question.next !== 'undefined') return question.next(profil)
+    }
+    previousPage(currentPage) {
+        const question = questions[currentPage]
+        if (typeof question.previous !== 'undefined') return question.previous()
     }
 }
 export default Questionnaire
