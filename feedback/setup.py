c8f7fd63b75058b18ed4d317208635907444f97b
Ronan Amicel <ronan.amicel@gmail.com>
2020-09-22 12:57:01 +0200

    Tech: remonte le "user agent" dans les retours utilisateurs
    Cela permettra d’identifier plus facilement des problèmes qui
    seraient liés à un navigateur web particulier.

--- feedback/setup.py
+++ feedback/setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="mesconseilscovid-feedback",
-    version="1.0.0",
+    version="1.1.0",
     url="https://github.com/Delegation-numerique-en-sante/mesconseilscovid",
     author="Délégation ministérielle du numérique en santé",
     author_email="mesconseilscovid@sante.gouv.fr",
     description="Retours utilisateurs pour Mes Conseils Covid",
     packages=["mesconseilscovid_feedback"],
     python_requires=">=3.7",
-    install_requires=["roll", "pykeybasebot"],
+    install_requires=["roll", "pykeybasebot", "user-agents"],
 )
