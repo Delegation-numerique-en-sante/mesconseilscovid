b8b8c6cee91ea47f9227f33c0d147c16dc04d87a
David Larlet <david@larlet.fr>
2020-10-07 15:04:57 -0400

    Tech: l’état du suivi ne stocke pas des booléens

--- src/scripts/page/suivisymptomes.js
+++ src/scripts/page/suivisymptomes.js
@@ -55,16 +55,17 @@ export default function suivisymptomes(form, app) {
         app.profil.ajouterEtat(etat)
 
         app.profil.symptomes_actuels = etat.symptomes
-        app.profil.symptomes_actuels_temperature = etat.fievre
+        app.profil.symptomes_actuels_temperature = etat.fievre === 'oui'
         app.profil.symptomes_actuels_temperature_inconnue = false
-        app.profil.symptomes_actuels_toux = etat.toux
+        app.profil.symptomes_actuels_toux = etat.toux === 'oui'
         app.profil.symptomes_actuels_odorat = false // ???
         app.profil.symptomes_actuels_douleurs =
             etat.etatGeneral === 'pire' || etat.etatGeneral === 'critique'
-        app.profil.symptomes_actuels_diarrhee = etat.diarrheeVomissements
+        app.profil.symptomes_actuels_diarrhee = etat.diarrheeVomissements === 'oui'
         app.profil.symptomes_actuels_fatigue =
             etat.etatGeneral === 'pire' || etat.etatGeneral === 'critique'
-        app.profil.symptomes_actuels_alimentation = etat.alimentationHydratation
+        app.profil.symptomes_actuels_alimentation =
+            etat.alimentationHydratation === 'oui'
         app.profil.symptomes_actuels_souffle =
             etat.essoufflement === 'pire' || etat.etatGeneral === 'critique'
 
