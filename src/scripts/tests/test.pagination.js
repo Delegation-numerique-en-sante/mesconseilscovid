af7cd879628e7fb5bf284403e38ed45df8b2ec63
David Larlet <david@larlet.fr>
2020-10-16 17:29:18 -0400

    Enlève cas particulier dans beforeSuiviSymptomes

--- src/scripts/tests/test.pagination.js
+++ src/scripts/tests/test.pagination.js
@@ -874,52 +874,6 @@ describe('Pagination', function () {
             })
             assert.isUndefined(beforeSuiviIntroduction(profil, questionnaire))
         })
-        it('redirige dépistage si profil complet mais pas de date', function () {
-            const profil = new Profil('mes_infos', {
-                departement: '34',
-                activite_pro: false,
-                activite_pro_public: false,
-                activite_pro_sante: false,
-                activite_pro_liberal: false,
-                foyer_enfants: true,
-                foyer_fragile: false,
-                age: '42',
-                grossesse_3e_trimestre: false,
-                poids: '70',
-                taille: '178',
-                antecedent_cardio: false,
-                antecedent_diabete: true,
-                antecedent_respi: false,
-                antecedent_dialyse: true,
-                antecedent_cancer: false,
-                antecedent_immunodep: false,
-                antecedent_cirrhose: false,
-                antecedent_drepano: false,
-                antecedent_chronique_autre: false,
-                symptomes_actuels: false,
-                symptomes_actuels_temperature: false,
-                symptomes_actuels_temperature_inconnue: false,
-                symptomes_actuels_toux: false,
-                symptomes_actuels_odorat: false,
-                symptomes_actuels_douleurs: false,
-                symptomes_actuels_diarrhee: false,
-                symptomes_actuels_fatigue: false,
-                symptomes_actuels_alimentation: false,
-                symptomes_actuels_souffle: false,
-                symptomes_actuels_autre: false,
-                symptomes_passes: false,
-                contact_a_risque: true,
-                contact_a_risque_meme_lieu_de_vie: false,
-                contact_a_risque_contact_direct: false,
-                contact_a_risque_actes: false,
-                contact_a_risque_espace_confine: false,
-                contact_a_risque_meme_classe: false,
-                contact_a_risque_stop_covid: false,
-                contact_a_risque_autre: true,
-                suivi_active: true,
-            })
-            assert.strictEqual(beforeSuiviSymptomes(profil, questionnaire), 'depistage')
-        })
         it('redirige suivi introduction vers algo orientation si profil non complet', function () {
             const profil = new Profil('mes_infos', {
                 departement: '34',
@@ -999,7 +953,10 @@ describe('Pagination', function () {
                 antecedent_drepano: false,
                 antecedent_chronique_autre: false,
             })
-            assert.strictEqual(beforeSuiviSymptomes(profil, questionnaire), 'depistage')
+            assert.strictEqual(
+                beforeSuiviSymptomes(profil, questionnaire),
+                'symptomesactuels'
+            )
         })
         it('redirige suivi historique vers algo orientation si profil non complet', function () {
             const profil = new Profil('mes_infos', {
