c58c0acaa384443bcc451c9e6960ba78a3db7387
Ronan Amicel <ronan.amicel@gmail.com>
2020-10-30 15:56:12 +0100

    Supprime les informations relatives au couvre-feu

--- contenus/conseils/conseils_couvre_feu_actif.md
+++
@@ -1,7 +0,0 @@
-<div class="conseil conseil-orange">
-
-<span class="nouveau">nouveau</span> Depuis le samedi 24 octobre, le couvre-feu s’applique dans votre département de 21 h à 6 h.
-
-</div>
-
-Pour vous déplacer pendant le couvre-feu, il vous faut une **« Attestation de déplacement dérogatoire »**. Vous pouvez [la remplir et la télécharger en ligne ici](https://media.interieur.gouv.fr/attestation-couvre-feu-covid-19/).
--- src/scripts/tests/test.data.js
+++ src/scripts/tests/test.data.js
@@ -1,6 +1,5 @@
 import { assert } from 'chai'
 
-import couvrefeu from '../data/couvrefeu.js'
 import incidence from '../data/incidence.js'
 import prefectures from '../data/prefectures.js'
 import departements from '../data/departements.js'
@@ -19,18 +18,7 @@ describe('Carte départements', function () {
         assert.isNumber(incidence['01'])
         assert.isAtLeast(incidence['01'], 0)
     })
-    it('Il y a le bon nombre de départements en couvre-feu', function () {
-        assert.strictEqual(
-            Object.values(couvrefeu).reduce((acc, current) => acc + current, 0),
-            54
-        )
-    })
-    it('On récupère le couvre-feu depuis le département', function () {
-        assert.isBoolean(couvrefeu['01'])
-        assert.isTrue(couvrefeu['01'])
-        assert.isFalse(couvrefeu['02'])
-    })
-    it('Il y a le bon nombre de liens vers les préféctures', function () {
+    it('Il y a le bon nombre de liens vers les préfectures', function () {
         assert.strictEqual(Object.keys(prefectures).length, 104)
     })
     it('On récupère le lien vers la préfecture depuis le département', function () {
