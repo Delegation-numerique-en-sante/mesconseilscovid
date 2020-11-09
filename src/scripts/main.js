c292787da3e8ca8685cdb846cfca0b9488e9e2c5
David Larlet <david@larlet.fr>
2020-11-09 10:46:26 -0500

    Tech: intégration du polyfill dans notre dépôt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,6 @@ import 'core-js/features/promise'
 
 // Polyfills pour les API du navigateur
 import 'core-js/web/url-search-params'
-import 'details-polyfill'
 import 'whatwg-fetch'
 import './polyfills/custom_event'
 
---
+++ src/scripts/polyfills/details_polyfill.js
@@ -0,0 +1,43 @@
+/* Inspired from https://github.com/rstacruz/details-polyfill */
+export default function applyDetailsSummaryPolyfill(element) {
+    const DETAILS = 'details'
+    const SUMMARY = 'summary'
+
+    function checkSupport() {
+        const el = document.createElement(DETAILS)
+        if (!('open' in el)) return false
+
+        el.innerHTML = '<' + SUMMARY + '>a</' + SUMMARY + '>b'
+        document.body.appendChild(el)
+
+        const diff = el.offsetHeight
+        el.open = true
+        const result = diff != el.offsetHeight
+
+        document.body.removeChild(el)
+        return result
+    }
+
+    const supported = checkSupport()
+    if (supported) {
+        return
+    }
+    // Click handler for `<summary>` tags
+    // eslint-disable-next-line no-extra-semi
+    ;[].forEach.call(element.querySelectorAll('details summary'), (summary) => {
+        summary.addEventListener('click', (event) => {
+            event.preventDefault()
+            let details = event.target
+            while (details.tagName.toLowerCase() !== DETAILS) {
+                details = details.parentElement
+            }
+            if (details.hasAttribute('open') || details.open) {
+                details.open = false
+                details.removeAttribute('open')
+            } else {
+                details.open = true
+                details.setAttribute('open', 'open')
+            }
+        })
+    })
+}
