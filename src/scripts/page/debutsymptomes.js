06252372a97190af739cca5f050c04dad0d5ae4f
Ronan Amicel <ronan.amicel@gmail.com>
2021-01-11 10:50:37 +0100

    Pré-remplit la date de début des symptômes si elle est connue

--- src/scripts/page/debutsymptomes.js
+++ src/scripts/page/debutsymptomes.js
@@ -4,32 +4,29 @@ import { getRadioValue } from '../formutils.js'
 import { joursAvant } from '../utils.js'
 
 export default function debutsymptomes(form, app) {
+    const submitButton = form.querySelector('input[type=submit]')
+
     // Si symptômes passés, on n’affiche pas aujourd’hui.
     if (app.profil.symptomes_passes) {
         hideElement(form.querySelector('#debut_symptomes_aujourdhui'))
         hideElement(form.querySelector('label[for="debut_symptomes_aujourdhui"]'))
     }
 
+    // Pré-remplir le formulaire avec les données du profil.
+    prefillForm(form, app.profil)
+    updateSubmitButton(form, submitButton)
+
     // eslint-disable-next-line no-extra-semi
     ;[].forEach.call(
         form.querySelectorAll('[name="suivi_symptomes_date"]'),
         (radio) => {
             radio.addEventListener('change', (event) =>
-                radioButtonChanged(form, event.target)
+                radioButtonChanged(form, event.target, submitButton)
             )
         }
     )
 
-    const datePicker = form.querySelector('#debut_symptomes_exacte')
-    datePicker.addEventListener('change', (event) => {
-        datePickerChanged(form, event.target)
-    })
-    // Autorise seulement un intervalle de dates (30 derniers jours).
-    const now = new Date()
-    datePicker.setAttribute('max', now.toISOString().substring(0, 10))
-    const trenteJoursAvant = joursAvant(30)
-    datePicker.setAttribute('min', trenteJoursAvant.toISOString().substring(0, 10))
-    addDatePickerPolyfill(datePicker, trenteJoursAvant, now)
+    setupDatePicker(form)
 
     form.addEventListener('submit', function (event) {
         event.preventDefault()
@@ -48,30 +45,77 @@ export default function debutsymptomes(form, app) {
     })
 }
 
-function radioButtonChanged(form, input) {
+function prefillForm(form, profil) {
+    if (typeof profil.symptomes_start_date !== 'undefined') {
+        if (profil.symptomes_start_date >= joursAvant(1)) {
+            form.elements['suivi_symptomes_date'].value = 'aujourdhui'
+        } else if (profil.symptomes_start_date >= joursAvant(2)) {
+            form.elements['suivi_symptomes_date'].value = 'hier'
+        } else if (profil.symptomes_start_date >= joursAvant(3)) {
+            form.elements['suivi_symptomes_date'].value = 'avant_hier'
+        } else if (profil.symptomes_start_date >= joursAvant(4)) {
+            form.elements['suivi_symptomes_date'].value = 'avant_avant_hier'
+        } else {
+            form.elements['suivi_symptomes_date'].value = 'encore_avant_hier'
+            let datePicker = form.elements['suivi_symptomes_date_exacte']
+            datePicker.value = profil.symptomes_start_date.toISOString().substr(0, 10)
+            datePicker.removeAttribute('disabled')
+        }
+    }
+}
+
+function setupDatePicker(form, submitButton) {
+    const datePicker = form.querySelector('#debut_symptomes_exacte')
+
+    datePicker.addEventListener('change', () => {
+        datePickerChanged(form, submitButton)
+    })
+
+    // Autorise seulement un intervalle de dates (30 derniers jours).
+    const now = new Date()
+    datePicker.setAttribute('max', now.toISOString().substring(0, 10))
+    const trenteJoursAvant = joursAvant(30)
+    datePicker.setAttribute('min', trenteJoursAvant.toISOString().substring(0, 10))
+
+    addDatePickerPolyfill(datePicker, trenteJoursAvant, now)
+}
+
+function radioButtonChanged(form, input, submitButton) {
+    updateDatePicker(form, input)
+    updateSubmitButton(form, submitButton)
+}
+
+function datePickerChanged(form, submitButton) {
+    updateSubmitButton(form, submitButton)
+}
+
+function updateDatePicker(form, input) {
     let datePicker = form.querySelector('#debut_symptomes_exacte')
-    let submitButton = form.querySelector('input[type="submit"]')
     if (input.value === 'encore_avant_hier') {
         datePicker.removeAttribute('disabled')
         datePicker.setAttribute('max', new Date().toISOString().slice(0, 10))
         datePicker.focus()
-        if (datePicker.value === '') {
-            submitButton.setAttribute('disabled', '')
-        } else {
-            submitButton.removeAttribute('disabled')
-        }
     } else {
         datePicker.setAttribute('disabled', '')
-        submitButton.removeAttribute('disabled')
     }
 }
 
-function datePickerChanged(form, input) {
-    let submitButton = form.querySelector('input[type="submit"]')
-    if (input.value === '') {
-        submitButton.setAttribute('disabled', '')
-    } else {
+function updateSubmitButton(form, submitButton) {
+    if (dateFromForm(form)) {
+        submitButton.value = 'Continuer'
         submitButton.removeAttribute('disabled')
+    } else {
+        submitButton.value = 'Veuillez remplir le formulaire au complet'
+        submitButton.setAttribute('disabled', '')
+    }
+}
+
+function dateFromForm(form) {
+    let radio = form.elements['suivi_symptomes_date']
+    if (radio.value === 'encore_avant_hier') {
+        return dateFromPicker(form.elements['suivi_symptomes_date_exacte'])
+    } else {
+        return dateFromRadioButton(radio)
     }
 }
 
