a692ee122704bc9ab35f149cc15af283634911e0
David Larlet <david@larlet.fr>
2021-01-12 10:26:08 -0500

    Tech: réutilise la liste des pages du questionnaire
    Lorsqu’on teste les pages qui sont interactives dans l’updater pour laisser finir le remplissage sans rediriger l’utilisateur·ice.

---
+++ src/scripts/tests/test.updater.js
@@ -0,0 +1,20 @@
+import { assert } from 'chai'
+
+import Updater from '../updater.js'
+
+describe('Updater', function () {
+    it('La page d’introduction n’est pas interactive', function () {
+        const updater = new Updater(undefined)
+        assert.isFalse(updater.onInteractivePage('introduction'))
+    })
+
+    it('La page de dépistage est interactive', function () {
+        const updater = new Updater(undefined)
+        assert.isTrue(updater.onInteractivePage('depistage'))
+    })
+
+    it('La page de suivi des symptômes est interactive', function () {
+        const updater = new Updater(undefined)
+        assert.isTrue(updater.onInteractivePage('suivisymptomes'))
+    })
+})
