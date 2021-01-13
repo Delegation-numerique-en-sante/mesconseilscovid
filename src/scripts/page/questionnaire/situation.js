f640b072dc1fd7fcbbc2c0f217e40f1f3cf7f4f4
Ronan Amicel <ronan.amicel@gmail.com>
2021-01-13 10:19:01 +0100

    Tech: réorganise le code JS de la page "situation"

--- src/scripts/page/questionnaire/situation.js
+++ src/scripts/page/questionnaire/situation.js
@@ -8,24 +8,38 @@ import {
 import geolocalisation from '../../geoloc.js'
 
 export default function situation(form, app) {
-    const button = form.querySelector('input[type=submit]')
+    // Pré-remplir le formulaire avec le profil.
     preloadForm(form, 'departement', app.profil)
     preloadCheckboxForm(form, 'foyer_enfants', app.profil)
     preloadCheckboxForm(form, 'activite_pro', app.profil)
     preloadCheckboxForm(form, 'activite_pro_sante', app.profil)
+
+    // Activer ou pas le bouton de validation.
+    const button = form.querySelector('input[type=submit]')
     const requiredLabel = app.profil.estMonProfil()
         ? 'Votre département de résidence est requis'
         : 'Son département de résidence est requis'
     toggleFormButtonOnSelectFieldsRequired(form, button.value, requiredLabel)
+
+    // Montrer les questions secondaires en cas d’activité pro.
     const primary = form.elements['activite_pro']
     enableOrDisableSecondaryFields(form, primary)
     primary.addEventListener('click', () =>
         enableOrDisableSecondaryFields(form, primary)
     )
+
+    // Bouton de géolocalisation.
+    document
+        .getElementById('geolocalisation')
+        .addEventListener('click', geolocalisation)
+
+    // Message d’erreur en cas de géolocalisation impossible.
     hideSelector(form, '#error-geolocalisation')
     form.querySelector('select').addEventListener('change', function () {
         hideSelector(form, '#error-geolocalisation')
     })
+
+    // Soumission du formulaire.
     form.addEventListener('submit', function (event) {
         event.preventDefault()
         app.profil.departement = event.target.elements['departement'].value
@@ -37,7 +51,4 @@ export default function situation(form, app) {
             app.goToNextPage('situation')
         })
     })
-    document
-        .getElementById('geolocalisation')
-        .addEventListener('click', geolocalisation)
 }
