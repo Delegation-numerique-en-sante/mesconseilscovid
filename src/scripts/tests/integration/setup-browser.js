9fde6ffd9e14bb02588bc373f9704605c1607528
Ronan Amicel <ronan.amicel@gmail.com>
2021-02-15 22:16:35 +0100

    Tech: facilite la mise au point des tests d’intégration
    Ne pas passer explicitement le paramètre `headless: true` permet
    d’afficher le navigateur en définissant une variable d’environnement
    pendant les tests d’intégration.

--- src/scripts/tests/integration/setup-browser.js
+++ src/scripts/tests/integration/setup-browser.js
@@ -3,10 +3,9 @@ import playwright from 'playwright'
 let browser
 
 before(async function () {
-    // Lance un navigateur « headless ».
-    browser = await playwright[process.env.npm_config_browser].launch({
-        headless: true,
-    })
+    // Lance un navigateur « headless » (définir la variable
+    // d’environnement PWDEBUG=1 pour afficher le navigateur)
+    browser = await playwright[process.env.npm_config_browser].launch()
 })
 
 after(async function () {
