e81b51e0e2d535ae356aaf5321125aeb09e7e5c3
Ronan Amicel <ronan.amicel@gmail.com>
2021-03-10 17:39:53 +0100

    Met à jour les conseils de vaccination >50 ans à risque

--- src/scripts/algorithme/vaccination.js
+++ src/scripts/algorithme/vaccination.js
@@ -8,8 +8,27 @@ export default class AlgorithmeVaccination {
         return this.profil.age >= 75
     }
 
+    isSup50() {
+        return this.profil.age >= 50
+    }
+
     isProfessionnelDeSante() {
         return this.profil.activite_pro_sante
+    }
+
+    isARisque() {
+        return (
+            this.profil.antecedent_cardio ||
+            this.profil.antecedent_diabete ||
+            this.profil.antecedent_respi ||
+            this.profil.antecedent_dialyse ||
+            this.profil.antecedent_greffe ||
+            this.profil.antecedent_cancer ||
+            this.profil.antecedent_immunodep ||
+            this.profil.antecedent_cirrhose ||
+            this.profil.antecedent_drepano ||
+            this.profil.antecedent_trisomie
+        )
     }
 
     isTresHautRisque() {
@@ -23,7 +42,10 @@ export default class AlgorithmeVaccination {
 
     isVaccinable() {
         return (
-            this.isSup75() || this.isProfessionnelDeSante() || this.isTresHautRisque()
+            this.isSup75() ||
+            this.isProfessionnelDeSante() ||
+            this.isTresHautRisque() ||
+            (this.isSup50() && this.isARisque())
         )
     }
 }
