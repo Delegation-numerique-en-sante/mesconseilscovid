c4ac78993e34be96d6bcdda419d0e4d5d7cac9e1
David Larlet <david@larlet.fr>
2021-03-16 11:48:17 -0400

    Liste les communes confinées le week-end (06 et 59)

--- contenus/conseils/conseils_confinement_week_end.md
+++ contenus/conseils/conseils_confinement_week_end.md
@@ -1,5 +1,5 @@
 <div class="conseil conseil-orange">
 
-<span class="nouveau">nouveau</span> Un **confinement** s’applique **le week-end** dans certaines communes de votre département. Il vous faudra [une attestation dérogatoire](https://media.interieur.gouv.fr/deplacement-covid-19/) pour justifier de vos sorties à l’extérieur de votre domicile, du **vendredi soir à 18 h** au **lundi matin à 6 h**.
+<span class="nouveau">nouveau</span> Un **confinement** s’applique **le week-end** dans tout votre département. Il vous faudra [une attestation dérogatoire](https://media.interieur.gouv.fr/deplacement-covid-19/) pour justifier de vos sorties à l’extérieur de votre domicile, du **vendredi soir à 18 h** au **lundi matin à 6 h**.
 
 </div>
--- src/scripts/algorithme/orientation.js
+++ src/scripts/algorithme/orientation.js
@@ -471,11 +471,13 @@ export default class AlgorithmeOrientation {
         const blockNames = ['conseils-vie-quotidienne']
         if (this.profil.departement === '976') {
             blockNames.push('conseils-confinement')
-        } else if (
-            this.profil.departement === '06' ||
-            this.profil.departement === '59' ||
-            this.profil.departement === '62'
-        ) {
+        } else if (this.profil.departement === '06') {
+            blockNames.push('conseils-couvre-feu-18h')
+            blockNames.push('conseils-confinement-week-end-06')
+        } else if (this.profil.departement === '59') {
+            blockNames.push('conseils-couvre-feu-18h')
+            blockNames.push('conseils-confinement-week-end-59')
+        } else if (this.profil.departement === '62') {
             blockNames.push('conseils-couvre-feu-18h')
             blockNames.push('conseils-confinement-week-end')
         } else {
