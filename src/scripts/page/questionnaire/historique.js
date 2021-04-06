86d36381339092d639be090695a4cc58f1a87d70
David Larlet <david@larlet.fr>
2021-04-06 14:56:03 -0400

    Facilite la sélection d’une ancienne date (historique)

--- src/scripts/page/questionnaire/historique.js
+++ src/scripts/page/questionnaire/historique.js
@@ -1,36 +1,46 @@
-import { addDatePickerPolyfill } from '../../datepicker'
 import {
+    createEvent,
     enableOrDisableSecondaryFields,
     preloadCheckboxForm,
     toggleFormButtonOnTextFieldsAndRadioRequired,
 } from '../../formutils'
+import { differenceEnJours, joursAvant } from '../../utils'
 
 export default function historique(form, app) {
-    // Autorise seulement une date passée.
-    const now = new Date()
-    const datePicker = form.querySelector('#covid_passee_date')
-    datePicker.setAttribute('max', now.toISOString().substring(0, 10))
-    addDatePickerPolyfill(datePicker, now)
-
+    const JOUR_PAR_MOIS = 30 // Approximation acceptable.
     // Remplir le formulaire avec les données du profil.
     preloadCheckboxForm(form, 'covid_passee', app.profil)
-    if (app.profil.covid_passee) {
-        if (typeof app.profil.covid_passee_date !== 'undefined') {
-            datePicker.value = app.profil.covid_passee_date
-                .toISOString()
-                .substring(0, 10)
-        }
+    if (typeof app.profil.covid_passee_date !== 'undefined') {
+        const difference = differenceEnJours(app.profil.covid_passee_date, new Date())
+        const toSelect = form.querySelector(
+            `input#covid_passee_date_${difference / JOUR_PAR_MOIS}_mois`
+        )
+        toSelect.checked = true
+        toSelect.dispatchEvent(createEvent('change'))
     }
 
     // La première case active ou désactive les autres.
-    var primary = form.elements['covid_passee']
+    const primary = form.elements['covid_passee']
     enableOrDisableSecondaryFields(form, primary)
     primary.addEventListener('click', () => {
         enableOrDisableSecondaryFields(form, primary)
     })
 
+    // On enrichit les choix dynamiquement avec les mois concernés.
+    ;[2, 3, 4, 5, 6].forEach((i) => {
+        const il_y_a_x_mois = joursAvant(JOUR_PAR_MOIS * i)
+        const month = il_y_a_x_mois.toLocaleString('default', { month: 'long' })
+        const label_x_mois = form.querySelector(
+            `label[for="covid_passee_date_${i}_mois"] span`
+        )
+        const suffix = i === 6 ? ' ou avant' : ''
+        label_x_mois.innerHTML = `${
+            label_x_mois.innerHTML
+        } (${month} ${il_y_a_x_mois.getFullYear()}${suffix})`
+    })
+
     // Le libellé du bouton change en fonction des choix.
-    var button = form.querySelector('input[type=submit]')
+    const button = form.querySelector('input[type=submit]')
     const uncheckedLabel = app.profil.estMonProfil()
         ? 'Je n’ai jamais eu la Covid'
         : 'Cette personne n’a jamais eu la Covid'
@@ -43,14 +53,13 @@ export default function historique(form, app) {
     )
 
     // Soumission du formulaire.
-    form.addEventListener('submit', function (event) {
+    form.addEventListener('submit', (event) => {
         event.preventDefault()
         const form = event.target
         app.profil.covid_passee = form.elements['covid_passee'].checked
         if (app.profil.covid_passee) {
-            app.profil.covid_passee_date = new Date(
-                form.elements['covid_passee_date'].value
-            )
+            const nbMonths = Number(form.elements['covid_passee_date'].value)
+            app.profil.covid_passee_date = joursAvant(nbMonths * 30)
         } else {
             app.profil.covid_passee_date = undefined
         }
