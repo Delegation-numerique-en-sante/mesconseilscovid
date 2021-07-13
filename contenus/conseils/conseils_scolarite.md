9b54292541e640a54c2e4fec878cfdb49509538c
David Larlet <david@larlet.fr>
2021-07-13 18:23:42 -0400

    Retrait des conseils sur les Études et la scolarité

--- contenus/conseils/conseils_scolarite.md
+++
@@ -1,6 +0,0 @@
-Concernant les **concours et les examens** :
-
-* les épreuves sont maintenues pour le **brevet**, le **bac général** (épreuve de philosophie et grand oral) et les **bac pro** ;
-* pour les **classes prépa** (CPGE), les concours auront lieu en présentiel ;
-* pour les **BTS** : maintien des épreuves terminales de juin, avec la part de stage et de contrôle continu, et une session exceptionnelle de rattrapage en juillet ;
-* pour tout le reste du supérieur, maintien des examens avec des sessions de rattrapage pour les élèves malades.
--- src/scripts/page/conseils.js
+++ src/scripts/page/conseils.js
@@ -155,7 +155,6 @@ export function showRelevantBlocks(element, profil, algoOrientation) {
     blockNames = blockNames.concat(algoOrientation.depistageBlockNamesToDisplay())
     blockNames = blockNames.concat(algoOrientation.vaccinBlockNamesToDisplay())
     blockNames = blockNames.concat(algoOrientation.gestesBarriereBlockNamesToDisplay())
-    blockNames = blockNames.concat(algoOrientation.scolariteBlockNamesToDisplay())
     blockNames = blockNames.concat(algoOrientation.vieQuotidienneBlockNamesToDisplay())
     blockNames = blockNames.concat(algoOrientation.activiteProBlockNamesToDisplay())
     blockNames = blockNames.concat(algoOrientation.grossesseBlockNamesToDisplay())
