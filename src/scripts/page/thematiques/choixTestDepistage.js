a80ef20868f0e381438a08760a6c0f42f68a21f5
Ronan Amicel <ronan.amicel@gmail.com>
2021-09-08 12:52:18 +0200

    Tech: structure le formulaire de choix du test de dépistage comme une classe

--- src/scripts/page/thematiques/choixTestDepistage.js
+++ src/scripts/page/thematiques/choixTestDepistage.js
@@ -5,125 +5,134 @@ import {
     uncheckAllRadio,
 } from '../../formutils'
 
-const FORMULAIRE = 'tests-de-depistage'
-
-const GESTIONNAIRES = {
-    'demarrage': gereDemarrage,
-    'symptomes': gereSymptomes,
-    'depuis-quand': gereDepuisQuand,
-    'cas-contact': gereCasContact,
-    'auto-test': gereAutoTest,
-}
-
 export function dynamiseLeChoixDuTest() {
-    const form = document.querySelector(`#${FORMULAIRE}-demarrage-form`)
-    gereDemarrage(form)
+    const formulaire = new Formulaire('tests-de-depistage')
+    formulaire.demarre()
 }
 
-function transitionneVersEtape(etape) {
-    const form = document.querySelector(`#${FORMULAIRE}-${etape}-form`)
-    showElement(form)
-    gereBoutonRetour(form)
-    const gestionnaire = GESTIONNAIRES[etape]
-    gestionnaire(form)
-}
+class Formulaire {
+    constructor(prefixe) {
+        this.prefixe = prefixe
+    }
 
-function gereBoutonRetour(form) {
-    const boutonRetour = form.querySelector('.back-button')
-    if (!boutonRetour) return
-    boutonRetour.addEventListener('click', (event) => {
-        event.preventDefault()
-        const etapePrecedente = boutonRetour.dataset.precedent
-        hideElement(form)
-        transitionneVersEtape(etapePrecedente)
-    })
-}
+    demarre(etape = 'demarrage') {
+        const form = document.querySelector(`#${this.prefixe}-${etape}-form`)
+        this.appelleGestionnaire(form, etape)
+    }
 
-function afficheReponse(etape) {
-    const reponse = document.querySelector(`#${FORMULAIRE}-${etape}-reponse`)
-    showElement(reponse)
-    gereBoutonRefaire()
-}
+    transitionneVersEtape(etape) {
+        const form = document.querySelector(`#${this.prefixe}-${etape}-form`)
+        showElement(form)
+        this.gereBoutonRetour(form)
+        this.appelleGestionnaire(form, etape)
+    }
 
-function gereBoutonRefaire() {
-    const boutonRefaire = document.querySelector(`#${FORMULAIRE}-refaire`)
-    showElement(boutonRefaire)
-    boutonRefaire.addEventListener('click', (event) => {
-        event.preventDefault()
-        hideElement(boutonRefaire)
-        hideSelector(document, '.statut')
-        uncheckAllRadio(document)
-        Array.from(
-            document.querySelectorAll('[data-initial-value]'),
-            (inputWithInitial) => {
-                inputWithInitial.value = inputWithInitial.dataset.initialValue
-                inputWithInitial.removeAttribute('data-initial-value')
-            }
-        )
-        transitionneVersEtape('symptomes')
-    })
-}
+    appelleGestionnaire(form, etape) {
+        const gestionnaire = this.GESTIONNAIRES[etape]
+        gestionnaire(form)
+    }
 
-function gereDemarrage(form) {
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        hideElement(form)
-        transitionneVersEtape('symptomes')
-    })
-}
+    gereBoutonRetour(form) {
+        const boutonRetour = form.querySelector('.back-button')
+        if (!boutonRetour) return
+        boutonRetour.addEventListener('click', (event) => {
+            event.preventDefault()
+            const etapePrecedente = boutonRetour.dataset.precedent
+            hideElement(form)
+            this.transitionneVersEtape(etapePrecedente)
+        })
+    }
 
-function gereSymptomes(form) {
-    const button = form.querySelector('input[type=submit]')
-    const requiredLabel = 'Cette information est requise'
-    toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const value = getRadioValue(form, 'tests_de_depistage_symptomes_radio')
-        hideElement(form)
-        if (value === 'oui') {
-            transitionneVersEtape('depuis-quand')
-        } else if (value === 'non') {
-            transitionneVersEtape('cas-contact')
-        }
-    })
-}
+    afficheReponse(etape) {
+        const reponse = document.querySelector(`#${this.prefixe}-${etape}-reponse`)
+        showElement(reponse)
+        this.gereBoutonRefaire()
+    }
 
-function gereDepuisQuand(form) {
-    const button = form.querySelector('input[type=submit]')
-    const requiredLabel = 'Cette information est requise'
-    toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const value = getRadioValue(form, 'tests_de_depistage_depuis_quand_radio')
-        hideElement(form)
-        afficheReponse(`symptomes-${value}`)
-    })
-}
+    gereBoutonRefaire() {
+        const boutonRefaire = document.querySelector(`#${this.prefixe}-refaire`)
+        showElement(boutonRefaire)
+        boutonRefaire.addEventListener('click', (event) => {
+            event.preventDefault()
+            hideElement(boutonRefaire)
+            hideSelector(document, '.statut')
+            uncheckAllRadio(document)
+            Array.from(
+                document.querySelectorAll('[data-initial-value]'),
+                (inputWithInitial) => {
+                    inputWithInitial.value = inputWithInitial.dataset.initialValue
+                    inputWithInitial.removeAttribute('data-initial-value')
+                }
+            )
+            this.transitionneVersEtape('symptomes')
+        })
+    }
 
-function gereCasContact(form) {
-    const button = form.querySelector('input[type=submit]')
-    const requiredLabel = 'Cette information est requise'
-    toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const value = getRadioValue(form, 'tests_de_depistage_cas_contact_radio')
-        hideElement(form)
-        if (value === 'oui') {
-            afficheReponse(`pas-symptomes-cas-contact-${value}`)
-        } else if (value === 'non') {
-            transitionneVersEtape('auto-test')
-        }
-    })
-}
-
-function gereAutoTest(form) {
-    const button = form.querySelector('input[type=submit]')
-    const requiredLabel = 'Cette information est requise'
-    toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        const value = getRadioValue(form, 'tests_de_depistage_auto_test_radio')
-        hideElement(form)
-        afficheReponse(`pas-symptomes-pas-cas-contact-auto-test-${value}`)
-    })
+    GESTIONNAIRES = {
+        'demarrage': (form) => {
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                hideElement(form)
+                this.transitionneVersEtape('symptomes')
+            })
+        },
+        'symptomes': (form) => {
+            const button = form.querySelector('input[type=submit]')
+            const requiredLabel = 'Cette information est requise'
+            toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                const value = getRadioValue(form, 'tests_de_depistage_symptomes_radio')
+                hideElement(form)
+                if (value === 'oui') {
+                    this.transitionneVersEtape('depuis-quand')
+                } else if (value === 'non') {
+                    this.transitionneVersEtape('cas-contact')
+                }
+            })
+        },
+        'depuis-quand': (form) => {
+            const button = form.querySelector('input[type=submit]')
+            const requiredLabel = 'Cette information est requise'
+            toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                const value = getRadioValue(
+                    form,
+                    'tests_de_depistage_depuis_quand_radio'
+                )
+                hideElement(form)
+                this.afficheReponse(`symptomes-${value}`)
+            })
+        },
+        'cas-contact': (form) => {
+            const button = form.querySelector('input[type=submit]')
+            const requiredLabel = 'Cette information est requise'
+            toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                const value = getRadioValue(
+                    form,
+                    'tests_de_depistage_cas_contact_radio'
+                )
+                hideElement(form)
+                if (value === 'oui') {
+                    this.afficheReponse(`pas-symptomes-cas-contact-${value}`)
+                } else if (value === 'non') {
+                    this.transitionneVersEtape('auto-test')
+                }
+            })
+        },
+        'auto-test': (form) => {
+            const button = form.querySelector('input[type=submit]')
+            const requiredLabel = 'Cette information est requise'
+            toggleFormButtonOnRadioRequired(form, button.value, requiredLabel)
+            form.addEventListener('submit', (event) => {
+                event.preventDefault()
+                const value = getRadioValue(form, 'tests_de_depistage_auto_test_radio')
+                hideElement(form)
+                this.afficheReponse(`pas-symptomes-pas-cas-contact-auto-test-${value}`)
+            })
+        },
+    }
 }
