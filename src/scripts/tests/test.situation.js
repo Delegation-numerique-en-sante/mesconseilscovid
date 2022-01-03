2c873113e169352b1d984d3eefab82063540b5cd
Ronan Amicel <ronan.amicel@gmail.com>
2022-01-03 16:37:19 +0100

    Même protocole cas contact pour les <12 ans que pour les vaccinés
    Source : https://solidarites-sante.gouv.fr/IMG/pdf/20220102_-_dgs-urgent_01-doctrines-isolement40n.pdf

--- src/scripts/tests/test.situation.js
+++ src/scripts/tests/test.situation.js
@@ -581,6 +581,23 @@ const matrice = [
         conseils: 'contact-a-risque-vaccine',
     },
     {
+        description: 'Négatif et contact à risque (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
+            contact_a_risque: true,
+            contact_a_risque_contact_direct: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'negatif',
+            _depistage_start_date: new Date().toJSON(),
+        },
+        situation: 'negatif_contact_a_risque_vaccine',
+        statut: 'contact-a-risque-vaccine',
+        conseils: 'contact-a-risque-vaccine',
+    },
+    {
         description: 'Négatif et contact à risque (pas complètement vacciné)',
         profil: {
             vaccins: 'pas_encore',
@@ -601,6 +618,23 @@ const matrice = [
             'Négatif et contact à risque même lieu de vie (complètement vacciné)',
         profil: {
             vaccins: 'completement',
+            contact_a_risque: true,
+            contact_a_risque_meme_lieu_de_vie: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'negatif',
+            _depistage_start_date: new Date().toJSON(),
+        },
+        situation: 'negatif_contact_a_risque_meme_lieu_de_vie_vaccine',
+        statut: 'contact-a-risque-meme-lieu-de-vie-vaccine',
+        conseils: 'contact-a-risque-meme-lieu-de-vie-vaccine',
+    },
+    {
+        description: 'Négatif et contact à risque même lieu de vie (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
             contact_a_risque: true,
             contact_a_risque_meme_lieu_de_vie: true,
             contact_a_risque_variante: 'aucune',
@@ -771,6 +805,23 @@ const matrice = [
         conseils: 'contact-a-risque-vaccine',
     },
     {
+        description: 'En attente et contact à risque (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
+            contact_a_risque: true,
+            contact_a_risque_contact_direct: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'en_attente',
+            _depistage_start_date: new Date().toJSON(),
+        },
+        situation: 'en_attente_contact_a_risque_vaccine',
+        statut: 'contact-a-risque-vaccine',
+        conseils: 'contact-a-risque-vaccine',
+    },
+    {
         description: 'En attente et contact à risque (pas complètement vacciné)',
         profil: {
             vaccins: 'pas_encore',
@@ -791,6 +842,24 @@ const matrice = [
             'En attente et contact à risque même lieu de vie (complètement vacciné)',
         profil: {
             vaccins: 'completement',
+            contact_a_risque: true,
+            contact_a_risque_meme_lieu_de_vie: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'en_attente',
+            _depistage_start_date: new Date().toJSON(),
+        },
+        situation: 'en_attente_contact_a_risque_meme_lieu_de_vie_vaccine',
+        statut: 'contact-a-risque-meme-lieu-de-vie-vaccine',
+        conseils: 'contact-a-risque-meme-lieu-de-vie-vaccine',
+    },
+    {
+        description:
+            'En attente et contact à risque même lieu de vie (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
             contact_a_risque: true,
             contact_a_risque_meme_lieu_de_vie: true,
             contact_a_risque_variante: 'aucune',
@@ -893,6 +962,20 @@ const matrice = [
         conseils: 'contact-a-risque-vaccine',
     },
     {
+        description: 'Pas testé et contact à risque (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
+            contact_a_risque: true,
+            contact_a_risque_contact_direct: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: false,
+        },
+        situation: 'pas_teste_contact_a_risque_vaccine',
+        statut: 'contact-a-risque-vaccine',
+        conseils: 'contact-a-risque-vaccine',
+    },
+    {
         description: 'Pas testé et contact à risque (guérison < 2 mois)',
         profil: {
             vaccins: 'pas_encore',
@@ -940,6 +1023,20 @@ const matrice = [
             'Pas testé et contact à risque même lieu de vie (complètement vacciné)',
         profil: {
             vaccins: 'completement',
+            contact_a_risque: true,
+            contact_a_risque_meme_lieu_de_vie: true,
+            contact_a_risque_variante: 'aucune',
+            depistage: false,
+        },
+        situation: 'pas_teste_contact_a_risque_meme_lieu_de_vie_vaccine',
+        statut: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+        conseils: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+    },
+    {
+        description: 'Pas testé et contact à risque même lieu de vie (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
             contact_a_risque: true,
             contact_a_risque_meme_lieu_de_vie: true,
             contact_a_risque_variante: 'aucune',
@@ -1074,6 +1171,23 @@ const matrice = [
         conseils: 'contact-a-risque-vaccine',
     },
     {
+        description: 'Test négatif obsolète et contact à risque (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'negatif',
+            _depistage_start_date: joursAvant(7),
+            contact_a_risque: true,
+            contact_a_risque_contact_direct: true,
+            contact_a_risque_variante: 'aucune',
+        },
+        situation: 'pas_teste_contact_a_risque_vaccine',
+        statut: 'contact-a-risque-vaccine',
+        conseils: 'contact-a-risque-vaccine',
+    },
+    {
         description:
             'Test négatif obsolète et contact à risque (pas complètement vacciné)',
         profil: {
@@ -1095,6 +1209,24 @@ const matrice = [
             'Test négatif obsolète et contact à risque même lieu de vie (complètement vacciné)',
         profil: {
             vaccins: 'completement',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'negatif',
+            _depistage_start_date: joursAvant(7),
+            contact_a_risque: true,
+            contact_a_risque_meme_lieu_de_vie: true,
+            contact_a_risque_variante: 'aucune',
+        },
+        situation: 'pas_teste_contact_a_risque_meme_lieu_de_vie_vaccine',
+        statut: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+        conseils: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+    },
+    {
+        description:
+            'Test négatif obsolète et contact à risque même lieu de vie (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
             depistage: true,
             depistage_type: 'rt-pcr',
             depistage_resultat: 'negatif',
@@ -1254,6 +1386,23 @@ const matrice = [
         conseils: 'contact-a-risque-vaccine',
     },
     {
+        description: 'Test positif obsolète et contact à risque (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'positif',
+            _depistage_start_date: joursAvant(30),
+            contact_a_risque: true,
+            contact_a_risque_contact_direct: true,
+            contact_a_risque_variante: 'aucune',
+        },
+        situation: 'pas_teste_contact_a_risque_vaccine',
+        statut: 'contact-a-risque-vaccine',
+        conseils: 'contact-a-risque-vaccine',
+    },
+    {
         description:
             'Test positif obsolète et contact à risque (pas complètement vacciné)',
         profil: {
@@ -1275,6 +1424,24 @@ const matrice = [
             'Test positif obsolète et contact à risque même lieu de vie (complètement vacciné)',
         profil: {
             vaccins: 'completement',
+            depistage: true,
+            depistage_type: 'rt-pcr',
+            depistage_resultat: 'positif',
+            _depistage_start_date: joursAvant(30),
+            contact_a_risque: true,
+            contact_a_risque_meme_lieu_de_vie: true,
+            contact_a_risque_variante: 'aucune',
+        },
+        situation: 'pas_teste_contact_a_risque_meme_lieu_de_vie_vaccine',
+        statut: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+        conseils: 'contact-a-risque-meme-lieu-de-vie-sans-depistage-vaccine',
+    },
+    {
+        description:
+            'Test positif obsolète et contact à risque même lieu de vie (moins de 12 ans)',
+        profil: {
+            age: 11,
+            vaccins: 'pas_encore',
             depistage: true,
             depistage_type: 'rt-pcr',
             depistage_resultat: 'positif',
